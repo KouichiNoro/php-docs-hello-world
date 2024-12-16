9abb75d67fc0b6b0b9f9c04540ea0f8e97982e9f
Koichi Noro <pleasure@sria.co.jp>
2024-12-16 21:04:45 +0900

    target change

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
 
 Version 0.0.1
 
-<form action="https://paypaytest01-c7gebhc9bndgdyg4.japaneast-01.azurewebsites.net/PayPayCreateQR.php" method="get" target="_blank">
+<form action="https://paypaytest01-c7gebhc9bndgdyg4.japaneast-01.azurewebsites.net/PayPayCreateQR.php" method="get" >
     <label>QRコード作成：</label>
     <input type="submit" value="作成" />
 </form>
 
-<form action="https://paypaytest01-c7gebhc9bndgdyg4.japaneast-01.azurewebsites.net/PayPayDeleteQR.php" method="get" target="_blank">
+<form action="https://paypaytest01-c7gebhc9bndgdyg4.japaneast-01.azurewebsites.net/PayPayDeleteQR.php" method="get" >
     <label>QRコード削除：</label><input type="text" name="codeId">
     <input type="submit" value="削除" />
 </form>
